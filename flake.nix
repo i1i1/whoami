23f41a2c7f01cc01868233c4311c438abae708be
i1i1 <vanyarybin1@live.ru>
2024-05-09 10:58:24 +0300

    Add simple site

--- flake.nix
+++ flake.nix
@@ -1,11 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+    terminimal-theme = {
+      url = "github:pawroman/zola-theme-terminimal";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, terminimal-theme }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        theme = terminimal-theme;
+        themeName = ((builtins.fromTOML (builtins.readFile "${theme}/theme.toml")).name);
       in
       {
         packages = rec {
@@ -14,6 +23,10 @@
             version = "2024-05-09";
             src = ./.;
             nativeBuildInputs = with pkgs; [ zola ];
+            configurePhase = ''
+              mkdir -p "themes/${themeName}"
+              cp -r ${theme}/* "themes/${themeName}"
+            '';
             buildPhase = "zola build";
             installPhase = "cp -r public $out";
           };
@@ -24,6 +37,11 @@
           packages = with pkgs; [
             zola
           ];
+          shellHook = ''
+            # TODO: `cd` into git root
+            mkdir -p themes
+            ln -sn "${theme}" "themes/${themeName}"
+          '';
         };
       }
     );
